29405ff415038cee34c16584059a649b546f0a44
varun thaker <vthakar3@gmail.com>
2023-12-19 19:11:44 +0100

    Added post request for subproducts and data for all end point

--- products/views.py
+++ products/views.py
@@ -1,4 +1,7 @@
 from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 from .models import Product
 from .models import Subcatergory
 from .models import Subproduct
@@ -10,17 +13,33 @@ def product_list(request):
     #get all proucts
     #serialize (frpm Python to JSON format)
     #return JSON
-
     products = Product.objects.all()
-    serialiser = ProductSerializer(products, many =True)
-    return JsonResponse(serialiser.data, safe = False)
+    serializer = ProductSerializer(products, many =True)
+    return JsonResponse({'products':serializer.data})
 
+@api_view(['GET', 'POST'])
 def subCatagory_list(request):
-    subCatagories = Subcatergory.objects.all()
-    serialiser = SubCatagorySerializer(subCatagories, many = True)
-    return JsonResponse(serialiser.data, safe = False)
+    if request.method == "GET":
+        subCatagories = Subcatergory.objects.all()
+        serializer = SubCatagorySerializer(subCatagories, many = True)
+        return JsonResponse({'subcatagories':serializer.data})
+    if request.method == "POST":
+        serializer = SubCatagorySerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
 
+
+@api_view(['GET', 'POST'])
 def subProduct_list(request):
-    subCatagories = Subproduct.objects.all()
-    serialiser = SubProductSerializer(subCatagories, many = True)
-    return JsonResponse(serialiser.data, safe = False)
+    
+    if request.method == "GET":
+        subProducts = Subproduct.objects.all()
+        serializer = SubProductSerializer(subProducts, many = True)
+        return JsonResponse({'subproducts':serializer.data})
+    
+    if request.method == "POST":
+        serializer = SubProductSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
